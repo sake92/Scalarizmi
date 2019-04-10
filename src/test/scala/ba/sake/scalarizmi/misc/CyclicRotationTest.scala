d7ed108d016b05f7ac9c1a33caf08db402cfca61
Sakib Hadziavdic <sake.etf@gmail.com>
2019-04-10 19:43:35 +0200

    Add PermMissingElem, Zirconium2019

--- src/test/scala/ba/sake/scalarizmi/misc/CyclicRotationTest.scala
+++ src/test/scala/ba/sake/scalarizmi/misc/CyclicRotationTest.scala
@@ -10,10 +10,10 @@ class CyclicRotationTest extends FlatSpec {
   Solutions.foreach { solution =>
     solution.toString should "rotate array right k places" in {
       List(
-        (Array(3, 8, 9, 7, 6) -> 3) -> Array(9, 7, 6, 3, 8),
-        (Array(1, 2, 3, 4) -> 4) -> Array(1, 2, 3, 4),
-        (Array(5, -1000) -> 1) -> Array(-1000, 5),
-        (Array.empty[Int] -> 0) -> Array.empty[Int]
+        (Array(3, 8, 9, 7, 6), 3) -> Array(9, 7, 6, 3, 8),
+        (Array(1, 2, 3, 4), 4) -> Array(1, 2, 3, 4),
+        (Array(5, -1000), 1) -> Array(-1000, 5),
+        (Array.empty[Int], 0) -> Array.empty[Int]
       ).foreach { case ((arr, k), expected) =>
         val sol = solution.solution(arr, k)
         sol should equal(expected)
